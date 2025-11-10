0ec34f5bc0cae1559d34548c64747e92534d903d
Herodes <a106927@alunos.uminho.pt>
2025-11-10 13:27:31 +0000

    isto não está correto, falta fazer funções mas não tenho tempo de complementar

---
+++ src/nave/servidor_tcp.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <boolean.h>
+
+typedef struct {
+    int socket;
+    int id_rover;
+    pthread_t thread;
+    bool ativo;
+} ConexaoRover;
+
+ConexaoRover conexoes[MAX_ROVERS];
+int num_conexoes = 0;
+pthread_mutex_t mutex_conexoes = PTHREAD_MUTEX_INITIALIZER;
+
+// Thread para cada rover
+void *thread_handler_rover(void *arg) {
+    ConexaoRover *conn = (ConexaoRover*)arg;
+    MissaoTCP msg;
+    
+    while (conn->ativo) {
+        ssize_t n = recv(conn->socket, &msg, sizeof(msg), 0);
+        if (n <= 0) {
+            printf("Rover %d desconectou\n", conn->id_rover);
+            break;
+        }
+        
+        // Processa telemetria ( FALTA FAZER ESTA FUNÇÃO)
+        processar_telemetria(&msg, conn->id_rover);
+        
+        // Atualiza estado interno ( FALTA FAZER ESTA FUNÇÃO)
+        atualizar_estado_rover(conn->id_rover, &msg);
+    }
+    
+    close(conn->socket);
+    conn->ativo = false;
+    return NULL;
+}
+
+// Aceita conexões TCP
+void servidor_tcp_telemetria(int porta) {
+    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(porta),
+        .sin_addr.s_addr = INADDR_ANY
+    };
+    
+    bind(server_sock, (struct sockaddr*)&addr, sizeof(addr));
+    listen(server_sock, 10);
+    
+    //DEBUG
+    // printf("Servidor TCP telemetria escuta na porta %d\n", porta);
+    
+    while (1) {
+        struct sockaddr_in client_addr;
+        socklen_t len = sizeof(client_addr);
+        int client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &len);
+        
+        pthread_mutex_lock(&mutex_conexoes);
+        
+        if (num_conexoes < MAX_ROVERS) {
+            ConexaoRover *conn = &conexoes[num_conexoes++];
+            conn->socket = client_sock;
+            conn->ativo = true;
+            pthread_create(&conn->thread, NULL, thread_handler_rover, conn);
+            pthread_detach(conn->thread);
+        }
+        
+        pthread_mutex_unlock(&mutex_conexoes);
+    }
+}
