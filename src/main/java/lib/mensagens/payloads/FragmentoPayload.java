cedbf74fd4b04c40c6b84d875fb8113b815f088a
beatriz0freitas <a106853@alunos.uminho.pt>
2025-11-23 10:37:30 +0000

    abstracao metodo serializar por campos

--- src/main/java/lib/mensagens/payloads/FragmentoPayload.java
+++ src/main/java/lib/mensagens/payloads/FragmentoPayload.java
@@ -1,10 +1,20 @@
 package lib.mensagens.payloads;
+import java.util.List;
+import java.util.ArrayList;
 
-    /**
-     * Classe auxiliar para armazenar fragmentos de dados.
-     */
-    public  class FragmentoPayload extends PayloadUDP {
-        public byte[] dados;
+/**
+ * Classe auxiliar para armazenar fragmentos de dados.
+ */
+public class FragmentoPayload extends PayloadUDP {
+    public byte[] dados;
 
+    /**Criei porque tornei o metodo abstrato - ainda por corrigir */
+    @Override
+    public List<byte[]> serializarPorCampos() {
+        List<byte[]> blocos = new ArrayList<>();
+        // TODO: serializar campos específicos
+        return blocos;
     }
+
+}
     
--- src/main/java/lib/mensagens/payloads/PayloadAck.java
+++ src/main/java/lib/mensagens/payloads/PayloadAck.java
@@ -1,6 +1,8 @@
 package lib.mensagens.payloads;
 
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import lib.mensagens.payloads.PayloadUDP;
 
 /**
@@ -20,4 +22,14 @@ public class PayloadAck extends PayloadUDP {
     public String toString() {
         return String.format("Ack{missingCount=%d, missing=%s}", missingCount, Arrays.toString(missing));
     }
+
+
+/**Criei porque tornei o metodo abstrato - ainda por corrigir */
+    @Override
+    public List<byte[]> serializarPorCampos() {
+        List<byte[]> blocos = new ArrayList<>();
+        // TODO: serializar campos específicos
+        return blocos;
+    }
+
 }
