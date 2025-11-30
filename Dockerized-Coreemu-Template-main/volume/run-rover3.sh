fb3c79032a698cc784aec99e55f970fbc90096dd
Alice-Soares <alice.faria.soares@gmail.com>
2025-11-30 19:45:03 +0000

    correção nos ips com topologia de teste para o core

--- Dockerized-Coreemu-Template-main/volume/run-rover3.sh
+++ Dockerized-Coreemu-Template-main/volume/run-rover3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Rover 3: id=3, x=0.0, y=10.0, porta=5001
-java -jar Rover.jar 3 0.0 10.0 5001
+# Rover 3: id=3, x=0.0, y=10.0, porta=5003, ipNave=10.0.0.1
+java -jar Rover.jar 3 0.0 10.0 5003 10.0.0.1
